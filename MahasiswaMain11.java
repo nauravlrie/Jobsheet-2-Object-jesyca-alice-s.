34e4c4358c6e5864c56aa969102201eb29155997
nauravlrie <nauravlrie@gmail.com>
2026-02-18 12:22:43 +0700

    kinerja

--- MahasiswaMain11.java
+++ MahasiswaMain11.java
@@ -10,13 +10,16 @@ public class MahasiswaMain11 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+        System.out.println("Nilai Kinerja: " + mhs1.nilaiKinerja());
 
         Mahasiswa11 mhs2 = new Mahasiswa11(" Annisa Nabila", "2141720160", 3.25, "TI 2L");
         mhs2.updateIpk(3.30);
         mhs2.tampilkanInformasi();
+        System.out.println("Nilai Kinerja: " + mhs2.nilaiKinerja());
 
         Mahasiswa11 mhs = new Mahasiswa11("Jesyca Alice", "254107060081", 3.90, "SIB 1A");
         mhs.tampilkanInformasi();
+        System.out.println("Nilai Kinerja: " + mhs.nilaiKinerja());
 
 
     }
